ba5d8e787bb5782337b4aff45625a44eb1a83e6e
glipR <37640160+glipR@users.noreply.github.com>
2020-11-26 14:52:17 +1100

    Add some old stuff for solution validation.

---
+++ solutions/Q2/P1/solution.py
@@ -0,0 +1,58 @@
+import os
+import itertools
+from dateutil import parser
+from ...read_data import read_dataset
+
+data = read_dataset("dataset/dataset")
+
+def solve(lines):
+    # First, get the number of practitioners.
+    n_pracs = int(lines[0])
+    prac_ids = [""] * n_pracs
+    solutions = [0] * n_pracs
+    for testno in range(n_pracs):
+        prac_ids[testno], serum = lines[testno+1].split()
+        serum = int(serum)
+        patients = {
+            id: pat for id, pat in data["patients"].items()
+            if prac_ids[testno] in pat.practitioners
+        }
+        for pat in patients:
+            pat.platelets = None
+        for obs in data["observations"].values():
+            if obs.code != "32623-1": continue
+            if not obs.patient in patients: continue
+            if patients[obs.patient].platelets is None or patients[obs.patient].platelets[1] < parser.parse(obs.effective):
+                patients[obs.patient].platelets = (
+                    (obs.value["value"], obs.value["unit"]),
+                    parser.parse(obs.effective),
+                )
+        saveable_patients = sorted([
+            pat for pat in patients.values()
+            if pat.platelets is not None
+        ], key=lambda pat: pat.platelets[0][0])
+
+        # Assert units are the same.
+        assert len(list(itertools.groupby(saveable_patients, lambda pat: pat.platelets[0][1]))) == 1, "More than one unit of measurement detected."
+
+        # saveable_patients are now sorted with platelet volume increasing. Save patients from the beginning of the list.
+        cur_index = 0
+        while cur_index < len(saveable_patients):
+            if serum >= saveable_patients[cur_index].platelets[0][0]:
+                serum -= saveable_patients[cur_index].platelets[0][0]
+            else:
+                break
+        solutions[testno] = cur_index
+    return "\n".join(
+        f"{prac_id} can save {sol} patients"
+        for prac_id, sol in zip(prac_ids, solutions)
+    )
+
+input_file_path = os.path.join(os.path.dirname(__file__), 'input.txt')
+output_file_path = os.path.join(os.path.dirname(__file__), 'correct_output.txt')
+
+with open(input_file_path, "r") as f:
+    inp = f.readlines()
+result = solve(inp)
+with open(output_file_path, "w") as f:
+    f.write(result)
