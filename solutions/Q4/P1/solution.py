832f2ac7bb485ce31a41aca39d229dadae9609a1
glipR <37640160+glipR@users.noreply.github.com>
2020-12-05 15:05:23 +1100

    Modify P1/P2 solutions.

--- solutions/Q4/P1/solution.py
+++ solutions/Q4/P1/solution.py
@@ -1,37 +1,39 @@
-import collections
 import os
 from ...read_data import read_dataset
 from dateutil import parser
 from collections import defaultdict
 
-data = read_dataset("dataset/build")
+NUM_COUPONS = 3
 
+def indiv_solve(d, org):
+    pracs = d["organizations"][org].practitioners
+    patient_info = {
+        # Number of encounters, and earliest encounter.
+        prac: defaultdict(lambda: (0, None))
+        for prac in pracs
+    }
+    for pat_id in d["organizations"][org].patients:
+        for encounter_id in d["patients"][pat_id].encounters:
+            encounter = d["encounters"][encounter_id]
+            for prac in encounter.practitioners:
+                patient_info[prac][pat_id] = (patient_info[prac][pat_id][0] + 1, patient_info[prac][pat_id][1])
+                if patient_info[prac][pat_id][1] is None or patient_info[prac][pat_id][1] > parser.parse(encounter.period_start):
+                    patient_info[prac][pat_id] = (patient_info[prac][pat_id][0], parser.parse(encounter.period_start))
+    all_patients = set()
+    for prac in pracs:
+        # Sort by encounters descending, then datetime ascending.
+        my_pats = sorted(list(patient_info[prac].items()), key=lambda kv: (-kv[1][0], kv[1][1], kv[0]))
+        for pat_id, value in my_pats[:NUM_COUPONS]:
+            all_patients.add(pat_id)
 
-def solve(lines):
+    return len(all_patients) == NUM_COUPONS * len(pracs)
+
+def solve(d, lines):
     n = int(lines[0])
     answers = []
     for testno in range(1, n + 1):
         org_id = lines[testno].strip()
-        pracs = data["organizations"][org_id].practitioners
-        patient_info = {
-            # Number of encounters, and earliest encounter.
-            prac: defaultdict(lambda: (0, None))
-            for prac in pracs
-        }
-        for encounter in data["encounters"].values():
-            if data["patients"][encounter.patient].organization != org_id: continue
-            for prac in encounter.practitioners:
-                patient_info[prac][encounter.patient] = (patient_info[prac][encounter.patient][0] + 1, patient_info[prac][encounter.patient][1])
-                if patient_info[prac][encounter.patient][1] is None or patient_info[prac][encounter.patient][1] > parser.parse(encounter.period_start):
-                    patient_info[prac][encounter.patient] = (patient_info[prac][encounter.patient][0], parser.parse(encounter.period_start))
-        all_patients = set()
-        for prac in pracs:
-            # Sort by encounters descending, then datetime ascending.
-            my_pats = sorted(list(patient_info[prac].items()), key=lambda kv: (-kv[1][0], kv[1][1], kv[0]))
-            for pat_id, value in my_pats[:3]:
-                all_patients.add(pat_id)
-
-        if len(all_patients) == 3 * len(pracs):
+        if indiv_solve(d, org_id):
             answers.append("POSSIBLE")
         else:
             answers.append("IMPOSSIBLE")
@@ -42,11 +44,13 @@ def solve(lines):
     )
 
 
-input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
-output_file_path = os.path.join(os.path.dirname(__file__), "correct_output.txt")
+if __name__ == "__main__":
+    data = read_dataset("dataset/build")
+    input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    output_file_path = os.path.join(os.path.dirname(__file__), "correct_output.txt")
 
-with open(input_file_path, "r") as f:
-    inp = f.readlines()
-result = solve(inp)
-with open(output_file_path, "w") as f:
-    f.write(result)
+    with open(input_file_path, "r") as f:
+        inp = f.readlines()
+    result = solve(data, inp)
+    with open(output_file_path, "w") as f:
+        f.write(result)
--- solutions/Q4/P2/solution.py
+++ solutions/Q4/P2/solution.py
@@ -1,107 +1,109 @@
-import collections
 import os
 from ...read_data import read_dataset
-from dateutil import parser
-from collections import defaultdict
 import queue
 
-data = read_dataset("dataset/build")
+NUM_COUPONS = 10
 
+def indiv_solve(d, org_id):
+    pracs = d["organizations"][org_id].practitioners
+    id_mapping = {
+        prac: i
+        for i, prac in enumerate(pracs)
+    }
+    patients = d["organizations"][org_id].patients
+    pat_edges = {
+        pat: set()
+        for pat in patients
+    }
+    for pat_id in patients:
+        for encounter_id in d["patients"][pat_id].encounters:
+            encounter = d["encounters"][encounter_id]
+            for prac in encounter.practitioners:
+                pat_edges[pat_id].add(prac)
 
-def solve(lines):
+    INF = int(1e9)
+    # U:= patients
+    # V:= pracs, with NUM_COUPONS nodes per prac.
+    PU = [None]*len(patients)
+    PV = [None]*(NUM_COUPONS*len(pracs))
+    dist = [0]*(len(patients)+1)
+    Q = queue.Queue()
+    NIL = len(patients)
+
+    def bfs():
+        for i in range(len(patients)):
+            if PU[i] == NIL:
+                dist[i] = 0
+                Q.put(i)
+            else:
+                dist[i] = INF
+        dist[NIL] = INF
+        while not Q.empty():
+            i = Q.get()
+            if dist[i] < dist[NIL]:
+                for prac in pat_edges[patients[i]]:
+                    j = id_mapping[prac]
+                    for k in range(NUM_COUPONS):
+                        index = k*len(pracs) + j
+                        if dist[PV[index]] == INF:
+                            dist[PV[index]] = dist[i] + 1
+                            Q.put(PV[index])
+        return dist[NIL] != INF
+    
+    def dfs(u):
+        if u != NIL:
+            for prac in pat_edges[patients[u]]:
+                j = id_mapping[prac]
+                for k in range(NUM_COUPONS):
+                    index = k*len(pracs) + j
+                    if dist[PV[index]] == dist[u] + 1:
+                        if dfs(PV[index]):
+                            PV[index] = u
+                            PU[u] = index
+                            return True
+            dist[u] = INF
+            return False
+        return True
+    
+    def hopcroft_karp():
+        for i in range(len(patients)):
+            PU[i] = NIL
+        for i in range(NUM_COUPONS*len(pracs)):
+            PV[i] = NIL
+        matching = 0
+        while bfs():
+            for i in range(len(patients)):
+                if PU[i] == NIL:
+                    if dfs(i):
+                        matching += 1
+        return matching
+
+    n_matches = hopcroft_karp()
+    return n_matches == NUM_COUPONS * len(pracs)
+
+def solve(d, lines):
     n = int(lines[0])
     answers = []
     for testno in range(1, n + 1):
         org_id = lines[testno].strip()
-        pracs = data["organizations"][org_id].practitioners
-        id_mapping = {
-            prac: i
-            for i, prac in enumerate(pracs)
-        }
-        patients = data["organizations"][org_id].patients
-        pat_edges = {
-            pat: set()
-            for pat in patients
-        }
-        for encounter in data["encounters"].values():
-            if data["patients"][encounter.patient].organization != org_id: continue
-            for prac in encounter.practitioners:
-                pat_edges[encounter.patient].add(prac)
-
-        INF = int(1e9)
-        # U:= patients
-        # V:= pracs, with 3 nodes per prac.
-        PU = [None]*len(patients)
-        PV = [None]*(3*len(pracs))
-        dist = [0]*(len(patients)+1)
-        Q = queue.Queue()
-        NIL = len(patients)
-
-        def bfs():
-            for i in range(len(patients)):
-                if PU[i] == NIL:
-                    dist[i] = 0
-                    Q.put(i)
-                else:
-                    dist[i] = INF
-            dist[NIL] = INF
-            while not Q.empty():
-                i = Q.get()
-                if dist[i] < dist[NIL]:
-                    for prac in pat_edges[patients[i]]:
-                        j = id_mapping[prac]
-                        for k in range(3):
-                            index = k*len(pracs) + j
-                            if dist[PV[index]] == INF:
-                                dist[PV[index]] = dist[i] + 1
-                                Q.put(PV[index])
-            return dist[NIL] != INF
-        
-        def dfs(u):
-            if u != NIL:
-                for prac in pat_edges[patients[u]]:
-                    j = id_mapping[prac]
-                    for k in range(3):
-                        index = k*len(pracs) + j
-                        if dist[PV[index]] == dist[u] + 1:
-                            if dfs(PV[index]):
-                                PV[index] = u
-                                PU[u] = index
-                                return True
-                dist[u] = INF
-                return False
-            return True
-        
-        def hopcroft_karp():
-            for i in range(len(patients)):
-                PU[i] = NIL
-            for i in range(3*len(pracs)):
-                PV[i] = NIL
-            matching = 0
-            while bfs():
-                for i in range(len(patients)):
-                    if PU[i] == NIL:
-                        if dfs(i):
-                            matching += 1
-            return matching
-
-        n_matches = hopcroft_karp()
-        if n_matches == 3 * len(pracs):
+        if indiv_solve(d, org_id):
             answers.append("POSSIBLE")
         else:
-            answers.append("IMPOSSIBLE")
+            answers.append("IMPOSSIBLE")        
 
     return "\n".join(
         f"Test {i + 1}: {answers[i]}"
         for i in range(n)
     )
 
+if __name__ == "__main__":
+    data = read_dataset("dataset/build")
 
-input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
-output_file_path = os.path.join(os.path.dirname(__file__), "correct_output.txt")
+    input_file_path = os.path.join(os.path.dirname(__file__), "input.txt")
+    output_file_path = os.path.join(os.path.dirname(__file__), "correct_output.txt")
 
-with open(input_file_path, "r") as f:
-    inp = f.readlines()
-result = solve(inp)
-with open(output_file_path, "w") as f:
-    f.write(result)
+    with open(input_file_path, "r") as f:
+        inp = f.readlines()
+    result = solve(data, inp)
+    with open(output_file_path, "w") as f:
+        f.write(result)
