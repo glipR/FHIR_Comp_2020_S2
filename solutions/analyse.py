a98aeb69a62dcff84d4703a57a3aa29dbd1515c6
glipR <37640160+glipR@users.noreply.github.com>
2020-12-05 14:15:25 +1100

    Ensure Q3 input is integer and ensure small differences don't affect the answer.

--- solutions/analyse.py
+++ solutions/analyse.py
@@ -1,6 +1,7 @@
 import os
 import time
 import random
+import math
 from .read_data import read_dataset
 
 s = time.time()
@@ -79,7 +80,7 @@ def generate_random_serum_amounts():
         serum = float(serum)
         # Choose anywhere from 0 to 1.01 * serum (small chance of having enough for everyone)
         # It seems like cases with about half the serum give the most interesting cases to break (Too little or too much and the answer is vert similar between p1/p2.)
-        actual_serum[i] = sit_middle(np.random.random()) * 1.01 * serum
+        actual_serum[i] = math.ceil(sit_middle(np.random.random()) * 1.01 * serum)
 
     res = f"{len(lines)}\n" + "\n".join(f"{lines[i].split()[0]} {actual_serum[i]}" for i in range(len(lines))) + "\n"
     with open("solutions/practitioners_serum_random.txt", "w") as f:
@@ -100,15 +101,39 @@ def q3_test_p1_p2_discrepancy(input_file):
             diff += 1
     print(f"{100 * diff / len(lines)}% of practitioners have different answers when considering civilization.")
 
+def q3_test_small_difference(input_file, diff):
+    # This is to test that very small differences in the serum don't change the correct number, to ensure that rounding/error accumulation don't affect the result.
+    from .Q3.P1.solution import indiv_solve as p1_solve
+    from .Q3.P2.solution import indiv_solve as p2_solve
+    with open(input_file, "r") as f:
+        lines = f.readlines()[1:]
+    p1_diff = 0
+    p2_diff = 0
+    for i, line in enumerate(lines):
+        prac, serum = line.split()
+        serum = float(serum)
+        p1 = p1_solve(data, prac, serum)
+        p2 = p2_solve(data, prac, serum)
+        p1_up = p1_solve(data, prac, serum + diff)
+        p2_up = p2_solve(data, prac, serum + diff)
+        p1_down = p1_solve(data, prac, serum - diff)
+        p2_down = p2_solve(data, prac, serum - diff)
+        p1_diff += (p1_up != p1 or p1 != p1_down)
+        p2_diff += (p2_up[0] != p2[0] or p2_up[1] != p2[1] or p2[0] != p2_down[0] or p2[1] != p2_down[1])
+
+    print(f"{100 * p1_diff / len(lines)}% of practitioners for P1 have different answers when considering {diff} changes in serum.")
+    print(f"{100 * p2_diff / len(lines)}% of practitioners for P2 have different answers when considering {diff} changes in serum.")
+
 # Function calls
 
 s = time.time()
 
-write_inputs()
+# write_inputs()
 # get_num_patients()
 # get_serum_values()
 # generate_random_serum_amounts()
 # q3_test_p1_p2_discrepancy("solutions/practitioners_serum_random.txt")
+# q3_test_small_difference("solutions/practitioners_serum_random.txt", 0.01)    # 0.01 makes no difference to the answer.
 
 e = time.time()
 
